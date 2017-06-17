1c9323e1a4eb8bff3368e5e387e71b93fd6b5b47
Viet Anh Nguyen <vietanh@vietanhdev.com>
2017-06-17 21:17:14 +0700

    make it better

--- init.el
+++ init.el
@@ -25,6 +25,9 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
